0d4e674583056787e1fa83fcf2f571486caed8dd
paydelimon22 <valentina2212.gonzalez@gmail.com>
2025-05-30 09:53:45 -0600

    chore(class10): file structure for class 10 example
    Copy the required files for class 10 examples in accordance to Milestone
    4.

---
+++ 501/vite-project/src/class10/A01027983_and_A01784875/Class10.tsx
@@ -0,0 +1,28 @@
+import Class10Example from "./Class10Example.tsx";
+import Button from "./components/Button.tsx";
+import { UserProvider } from "./components/userContext.tsx";
+import "./Class10.css"
+
+function Class10() {
+  return (
+    <>
+      <div>
+        <h1>Menu</h1>
+        <div>
+          <Button label="Main Page" onClick={() => document.location= "/"} />
+          <Button label="Milestone1" onClick={() => document.location = "/src/class1/A01027983/"} />
+          <Button label="Milestone2" onClick={() => document.location = "/src/class4/A01027983_and_A01784875/"} />
+          <Button label="Class7" />
+          <Button label="Class8" onClick={() => document.location = "/src/class8/A01027983_and_A01784875/"} />
+          <Button label="Milestone4" onClick={() => document.location = "/src/class9/A01027983_and_A01784875/"} />
+        </div>
+      </div>
+      <h1>Class 7 Example A01027983 and A01784875</h1>
+      <UserProvider>
+        <Class10Example />
+      </UserProvider>
+    </>
+  )
+}
+
+export default Class10;
